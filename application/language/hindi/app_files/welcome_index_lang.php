0fc9b91f0b8041aeedcbf8d28a98d63ab3fb5b43
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-24 21:38:26 +0530

    update

---
+++ application/language/hindi/app_files/welcome_index_lang.php
@@ -0,0 +1,2 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['homepage_welcome'] = 'हमारी वैबसाइट पर आपका स्वागत है। हमें यकीन है कि आप यहाँ क्या पाएंगे पसंद करेंगे।.';
